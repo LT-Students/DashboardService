43edc7cc23038dc219dc542d12ab624a580430d3
Glunky <vkontakte13011997@gmail.com>
2023-12-14 20:34:51 +0300

    add validation

---
+++ src/DashboardService.Validation/Priority/CreatePriorityRequestValidator.cs
@@ -0,0 +1,16 @@
+using FluentValidation;
+using LT.DigitalOffice.DashboardService.Data.Interfaces;
+using LT.DigitalOffice.DashboardService.Models.Dto.Requests.Priority;
+using LT.DigitalOffice.DashboardService.Validation.Priority.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LT.DigitalOffice.DashboardService.Validation.Priority;
+
+public class CreatePriorityRequestValidator : AbstractValidator<CreatePriorityRequest>, ICreatePriorityRequestValidator
+{
+
+  public CreatePriorityRequestValidator([FromServices] IPriorityRepository reposisoty)
+  {
+    
+  }
+}
