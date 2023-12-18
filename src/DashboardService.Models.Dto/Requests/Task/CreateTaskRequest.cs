76aaaa96cd76b7567fb59f9c192329a8de8a5e93
Glunky <vkontakte13011997@gmail.com>
2023-12-18 21:17:39 +0300

    added required attr to name of CreateTaskRequest

--- src/DashboardService.Models.Dto/Requests/Task/CreateTaskRequest.cs
+++ src/DashboardService.Models.Dto/Requests/Task/CreateTaskRequest.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace LT.DigitalOffice.DashboardService.Models.Dto.Requests.Task;
 
@@ -7,6 +8,7 @@ public record CreateTaskRequest
   public Guid GroupId { get; set; }
   public Guid? TaskTypeId { get; set; }
   public Guid? PriorityId { get; set; }
+  [Required]
   public string Name { get; set; }
   public string Content { get; set; }
   public DateTime? DeadlineAtUtc { get; set; }
